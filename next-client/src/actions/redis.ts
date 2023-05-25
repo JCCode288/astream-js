30bbfba8fb5eda5513f0036d2f9c4209e161c821
JCCode28 <jendycaprico@gmail.com>
2023-05-25 19:20:06 +0700

    fix: redis config delete username

--- next-client/src/actions/redis.ts
+++ next-client/src/actions/redis.ts
@@ -10,9 +10,7 @@ if (
 const config = {
   port: Number(process.env.REDIS_PORT) || 6379, // Redis port
   host: process.env.REDIS_HOST || "localhost", // Redis host
-  username: process.env.REDIS_USERNAME || "redis", // needs Redis >= 6
-  password: process.env.REDIS_PASSWORD || "password",
-  db: 0, // Defaults to 0
+  password: process.env.REDIS_PASSWORD || "",
 };
 
 const redis = new Redis(config);
