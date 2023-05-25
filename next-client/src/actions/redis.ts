d99393ff1f435d07fecb4bed4416aab49c7d6c69
JCCode28 <jendycaprico@gmail.com>
2023-05-25 18:56:31 +0700

    feat: add redis caching

---
+++ next-client/src/actions/redis.ts
@@ -0,0 +1,17 @@
+import { Redis } from "ioredis";
+import dotenv from "dotenv";
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+const config = {
+  port: Number(process.env.REDIS_PORT) || 6379, // Redis port
+  host: process.env.REDIS_HOST || "localhost", // Redis host
+  username: process.env.REDIS_USERNAME || "redis", // needs Redis >= 6
+  password: process.env.REDIS_PASSWORD || "password",
+  db: 0, // Defaults to 0
+};
+
+const redis = new Redis(config);
+
+export default redis;
