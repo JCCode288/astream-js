40b6df20b52b1e99b522123646ca888080401e19
JCCode28 <jendycaprico@gmail.com>
2023-06-12 22:37:32 +0700

    fix: logic create comment feat: add edit comment

--- next-client/src/actions/comments.ts
+++ next-client/src/actions/comments.ts
@@ -12,17 +12,40 @@ interface ICommentDTO {
 
 export const createComment = async (commentData: ICommentDTO) => {
   try {
-    let { comment, watching_id, rating } = commentData;
+    let { comment, user_id, watching_id, rating } = commentData;
 
     if (!comment) throw new Errors(400, "Comment is empty");
 
-    if (!watching_id) throw new Errors(400, "Watching Id is undefined");
+    if (!watching_id || !user_id)
+      throw new Errors(400, "Watching Id or User Id is undefined");
 
     let createdComment = await prisma.comment.create({
       data: commentData,
     });
 
     let message = `comment has been added`;
+
+    return { message };
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const editComment = async (commentData: ICommentDTO, id: number) => {
+  try {
+    let { comment, user_id, watching_id, rating } = commentData;
+
+    if (!comment) throw new Errors(400, "Comment is empty");
+
+    if (!watching_id || !user_id)
+      throw new Errors(400, "Watching Id or User Id is undefined");
+
+    let updatedComment = await prisma.comment.update({
+      where: { id },
+      data: commentData,
+    });
+
+    let message = `Your comment has been updated`;
 
     return { message };
   } catch (err) {
